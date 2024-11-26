727df61be923678b851c35707c6da2a0bb3ba39a
GameCod1ng <hyeongseok36@naver.com>
2024-11-26 16:17:49 +0900

    11.26 1차

--- src/main/java/webproject/easydent/controller/DentistryController.java
+++ src/main/java/webproject/easydent/controller/DentistryController.java
@@ -3,6 +3,7 @@ package webproject.easydent.controller;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
